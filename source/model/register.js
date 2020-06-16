66c45051d1e72ccd34126a2f26d988d069973d12
moayada <moayada@kth.se>
2020-06-16 17:54:04 +0200

    getting info from user registeration and save it in user object

--- source/model/register.js
+++ source/model/register.js
@@ -1,3 +1,15 @@
 import hello from '../server/registers.js';
 
-console.log(hello);
+document.querySelector('#reg').addEventListener('click', ()=>{
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    let confirm = document.querySelector('#confirm').value;
+    let user = {
+        'name': name,
+        'email': email,
+        'password': password,
+        'confirmed password': confirm
+    };
+    console.log(user);
+})
